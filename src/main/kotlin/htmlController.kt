04fa1d8ab95728a85a2bd68700999254191fa19e
ositlar <96839799+ositlar@users.noreply.github.com>
2022-05-22 19:13:16 +0600

    Update htmlController.kt

--- src/main/kotlin/htmlController.kt
+++ src/main/kotlin/htmlController.kt
@@ -15,17 +15,17 @@ class htmlController (file: Document) {
         return groups
     }
     fun tableAnalysis (file: Document): TimeTable {
-        val timeTable = TimeTable(mutableListOf(), mutableListOf())
-        var lessonToday: Lesson?
-        //var rowIterator = 2
-        val lastRow = rows.size - 1
-        var upperWeekDay: Day
+        val timeTable = TimeTable(mutableListOf(), mutableListOf()) //Создания объекта расписания
+        var lessonToday: Lesson? //Создание объекта пары, которую обрабатываем в данный момент
+        val lastRow = rows.size - 1 //Определяем последнюю строку, затем на основе этого значения высчитываем принадлежность дня недели к верхней или нижней неделе
+        var upperWeekDay: Day //Создание объектов дня недели для каждого типа недели
         var lowerWeekDay: Day
-        val lastCell = 6
-        var i = 1
-        for (rowIterator in 2 until 14) {
-            upperWeekDay = Day(rows[rowIterator].select("td")[0].text(), mutableListOf())
+        val lastCell = 6 //Максимум 5 пар в день, поэтому работаем со столбцами 0-5, где 1-5 - пары, 0 - название дня недели
+        var i = 1 //Итератор столбцов
+        for (rowIterator in 2 until 14) {       //Индексы строк начинаютсяя с 0, таблица заканчивается на 14 строке, то есть на 13-ом индексе
+            upperWeekDay = Day(rows[rowIterator].select("td")[0].text(), mutableListOf()) //Заносим в объекты название дня недели
             lowerWeekDay = Day(rows[rowIterator].select("td")[0].text(), mutableListOf())
+            var i = 1 //Итератор столбцов
             while (i < lastCell) {
                 val coll = rows[rowIterator].select("td")
                 if (coll[i].text().filter { !it.isWhitespace() } != "") {
@@ -70,16 +70,15 @@ class htmlController (file: Document) {
                 } else {
                     lessonToday = null
                 }
-                if ((lastRow - rowIterator) >= 6 ) {
+                if ((lastRow - rowIterator) >= 6 ) { //Определяем тип недели
                     upperWeekDay.classes.add(lessonToday)
                 } else {
                     lowerWeekDay.classes.add(lessonToday)
                 }
-                timeTable.upperWeek.add(upperWeekDay)
+                timeTable.upperWeek.add(upperWeekDay) //Заносим обработанную неделю в расписание
                 timeTable.lowerWeek.add(lowerWeekDay)
                 i++
             }
-            i = 1
         }
         return timeTable
     }
